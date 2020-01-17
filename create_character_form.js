706fe1bd4383cdbb8c879a7c0601d9ef3332cfda
sergey <sorg.true@gmail.com>
2020-01-17 18:23:01 +0300

    Updates

--- create_character_form.js
+++ create_character_form.js
@@ -1,3 +1,4 @@
+// Choose Race
 getAcc();
 function myRace() {
   let description = document.getElementsByClassName("race-description")[0];
@@ -37,7 +38,16 @@ function myRace() {
     `;
     traits.innerHTML = `
     <p>+1 to All Ability Scores</p>
-    <p>Extra Language</p>
+    <label for="language">Extra Language</label>
+    <select name="language" id="language">
+      <option value="none">- Choose a Language -</option>
+      <option value="none">Abyssal</option>
+      <option value="none">Draconic</option>
+      <option value="none">Elvish</option>
+      <option value="none">Goblin</option>
+      <option value="none">Halfling</option>
+      <option value="none">Orc</option>
+    </select>
     `;
     getAcc();
   }
@@ -77,6 +87,29 @@ function myRace() {
     <p>Keen Senses</p>
     <p>Fey Ancestry</p>
     <p>Trance</p>
+    <label for="cantrip">Cantrip</label>
+    <br/>
+    <select name="cantrip" id="cantrip">
+      <option value="none">- Choose a Spell -</option>
+      <option value="ctrlFlames">Control Flames</option>
+      <option value="gust">Gust</option>
+      <option value="frostBite">Frost Bite</option>
+      <option value="light">Light</option>
+      <option value="mirIllusion">Minor Illusion</option>
+      <option value="trueStrike">True Strike</option>
+    </select>
+    <br/>
+    <label for="language">Extra Language</label>
+    <br/>
+    <select name="language" id="language">
+      <option value="none">- Choose a Language -</option>
+      <option value="abyssal">Abyssal</option>
+      <option value="draconic">Draconic</option>
+      <option value="elvish">Elvish</option>
+      <option value="goblin">Goblin</option>
+      <option value="halfling">Halfling</option>
+      <option value="orc">Orc</option>
+    </select>
     `;
     getAcc();
   }
@@ -152,6 +185,14 @@ function myRace() {
     <p>+2 Constitution</p>
     <p>Darkvision</p>
     <p>Dwarven Resilience</p>
+    <label for="toolProf">Tool Proficiency</label>
+    <br/>
+    <select name="toolProf" id="toolProf">
+      <option value="none">- Choose a Dwarf Artisan's Tool -</option>
+      <option value="brewer">Brewer's Supplies</option>
+      <option value="mason">Mason's Tools</option>
+      <option value="smith">Smith's Tools</option>
+    </select>
     <p>Dwarven Combat Training</p>
     <p>Stonecunning</p>
     `;
@@ -159,6 +200,97 @@ function myRace() {
   }
 }
 
+// Choose Class
+function myClass() {
+  let description = document.getElementsByClassName("class-description")[0];
+  let img = document.getElementById("class-img");
+  let classes = document.getElementById("classes");
+  let features = document.getElementsByClassName("features")[0];
+  if (classes.value === "fighter") {
+    img.src = "./img/form_img/fighter.jpeg";
+    features.innerHTML = `
+    <h3>Class features</h3>
+    <p><span class="main-color">Hit Die:</span> d10</p>
+    <p><span class="main-color">Primary Ability:</span> Strength or Dexterity</p>
+    <p><span class="main-color">Saves:</span> Strength & Constitution</p>
+    `;
+    description.innerHTML = `
+    <h4>Description:</h4>
+    <p>
+      A master of martial combat, skilled with a variety of weapons and armor
+    </p>
+    <button class="accordion">Hit Points</button>
+    <div class="acc-body">
+      <p><span class="main-color">Hit Dice:</span> 1d10 per fighter level</p>
+      <p><span class="main-color">Hit Points at 1st Level:</span"> 10 + your Constitution modifier</p>
+      <p><span class="main-color">Hit Points at Higher Levels:</span> 1d10 (or 6) + your Constitution modifier per fighter level after 1st</p>
+    </div>
+    <button class="accordion points">Proficiencies</button>
+    <div class="acc-body">
+      <p><span class="main-color">Armor:</span> All armor, shields</p>
+      <p><span class="main-color">Weapons:</span"> Simple weapons, martial weapons</p>
+      <p><span class="main-color">Tools:</span> None</p>
+      <p><span class="main-color">Saving Throws:</span> Strength, Constitution</p>
+      <p><span class="main-color">Skills:</span> Choose two skills from Acrobatics, Animal Handling, Athletics, History, Insight, Intimidation, Perception, and Survival</p>
+      <select name="fighter-skill" id="fighter-skill">
+        <option value="none">- Choose a Fighter Skill -</option>
+        <option value="acrobatics">Acrobatics</option>
+        <option value="animalHandling">Animal Handling</option>
+        <option value="athletics">Athletics</option>
+        <option value="history">History</option>
+        <option value="insight">Insight</option>
+        <option value="intimidation">Intimidation</option>
+        <option value="perception">Perception</option>
+        <option value="survival">Survival</option>
+      </select>
+      <br>
+      <select name="fighter-skill" id="fighter-skill">
+        <option value="none">- Choose a Fighter Skill -</option>
+        <option value="acrobatics">Acrobatics</option>
+        <option value="animalHandling">Animal Handling</option>
+        <option value="athletics">Athletics</option>
+        <option value="history">History</option>
+        <option value="insight">Insight</option>
+        <option value="intimidation">Intimidation</option>
+        <option value="perception">Perception</option>
+        <option value="survival">Survival</option>
+      </select>
+    </div>
+    <button class="accordion points">Fighting Style</button>
+    <div class="acc-body">
+      <p>
+        You adopt a particular style of fighting as your specialty. Choose one of the following options. You can’t take a Fighting Style option more than once, even if you later get to choose again.
+      </p>
+      <select name="fighter-style" id="fighter-style">
+        <option value="none">- Choose an Option -</option>
+        <option value="archery">Archery</option>
+        <option value="defence">Defence</option>
+        <option value="dueling">Dueling</option>
+        <option value="greatWeapon">Great Weapon Fighting</option>
+        <option value="protection">Protection</option>
+        <option value="twoWeapon">Two-Weapon Fighting</option>
+      </select>
+    </div>
+    <button class="accordion">Second Wind</button>
+    <div class="acc-body">
+      <p>
+        You have a limited well of stamina that you can draw on to protect yourself from harm.
+        On your turn, you can use a bonus action to regain hit points equal to 1d10 + your fighter level.
+        Once you use this feature, you must finish a short or long rest before you can use it again.
+      </p>
+    </div>
+    `;
+  }
+  if (classes.value === "ranger") {
+    img.src = "./img/form_img/ranger.jpeg";
+  }
+  if (classes.value === "rogue") {
+    img.src = "./img/form_img/rogue.jpeg";
+  }
+  if (classes.value === "sorcerer") {
+    img.src = "./img/form_img/sorcerer.jpeg";
+  }
+}
 // Accordion
 
 function getAcc() {
@@ -175,4 +307,42 @@ function getAcc() {
       }
     });
   }
+}
+
+// Multiple steps functionality
+let currentTab = 0; // Current tab is set to be the first tab
+showTab(currentTab); // Display the current tab
+
+// This function will display the specified tab of the form
+function showTab(n) {
+  let tab = document.getElementsByClassName("tab");
+  tab[n].style.display = "block";
+  // fix the previous and next button
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline-block";
+  }
+  if (n == tab.length - 1) {
+    document.getElementById("nextBtn").innerHTML = "Create";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+}
+
+// This function will figure out which  tab to display
+function nextPrev(n) {
+  let tab = document.getElementsByClassName("tab");
+  // Hide the current tab
+  tab[currentTab].style.display = "none";
+  // increace or decrease the current tab by 1
+  currentTab += n;
+  // if you have reached the end of the form...
+  if (currentTab >= tab.length) {
+    // .. submit the form
+    document.getElementById("ch-form").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab
+  showTab(currentTab);
 }
